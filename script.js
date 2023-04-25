7ff7e4344e74bf5336105ada569ef9b0e7a1457e
FarrelHI <farrel.indallah@gmail.com>
2023-04-25 19:24:45 +0700

    finish the function that contains playerSelection & computerSelection

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
 let errorMessage = "Something seems to be wrong.... Let me fix it first.";
 
-
 function computerPlay(){
     let computerNumberChoice = Math.floor(Math.random()*3); //Randomize a number form 0 to 2
     let computerStringChoice = "";
 
     switch(computerNumberChoice){ //Convert the number to string
         case 0:
-            computerStringChoice = "Rock";
-            break;
+            return computerStringChoice = "rock";
         case 1:
-            computerStringChoice = "Paper";
-            break;
+            return computerStringChoice = "paper";
         case 2:
-            computerStringChoice = "Scissors";
-            break;
+            return computerStringChoice = "scissors";
         default:
-            return errorMessage;
+            return console.log(errorMessage);
     } 
-    return computerStringChoice;
 }
 
 function playRound(playerSelection, computerSelection){
     // Code Here
+    lowerTextPlayerSelection = playerSelection.toLowerCase();
+
+    if (computerSelection == lowerTextPlayerSelection){
+        return console.log("It's a Tie!!");
+    }
+    else if (lowerTextPlayerSelection == "rock" && computerSelection == "paper"){
+        return console.log("You Lose! Paper beats Rock");
+    }
+    else if (lowerTextPlayerSelection == "rock" && computerSelection == "scissors"){
+        return console.log("You Win! Rock beats Scissors");
+    }
+    else if (lowerTextPlayerSelection == "paper" && computerSelection == "scissors"){
+        return console.log("You Lose! Paper beats Scissors");
+    }
+    else if (lowerTextPlayerSelection == "paper" && computerSelection == "rock"){
+        return console.log("You Win! Paper beats Rock");
+    }
+    else if (lowerTextPlayerSelection == "scissors" && computerSelection == "paper"){
+        return console.log("You Win! Scissors beats Paper");
+    }
+    else if (lowerTextPlayerSelection == "scissors" && computerSelection == "rock"){
+        return console.log("You Lose! Rock beats Scissors");
+    }
+    else{
+        return console.log(errorMessage);
+    }
 }
+
+// Check each function
+// console.log(computerPlay()); //Check computerPlay output
+// playRound("RoCk", computerPlay()); //Check playRound output
 
 function game(){
     // Code Here
