b0027b1f345297fd4fdcdbf9ca0264c2b523c931
FarrelHI <farrel.indallah@gmail.com>
2023-04-29 15:40:15 +0700

    Add more function & fix cancel button
    cancel button will now ends the game

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function computerPlay(){
     } 
 }
 
-function playRound(playerSelection, computerSelection){
+function processingTheRoundWinner(playerSelection, computerSelection){
     if (computerSelection == playerSelection){
         userTieCount +=1;
         return "It's a Tie!!";
@@ -55,21 +55,22 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){    
-    alert("Time to Play Rock-Paper-Scissors!!\nThis is going to be a Five Round Match againts a Computer\nAre you Ready??");
+function playFiveRound(){
     for (let i=0;i<5;i++){
         alert(`Round ${i+1}!\n[Win: ${userWinCount}, Lose: ${userLoseCount}, Tie: ${userTieCount}]`);
         let userGameInput = prompt("Choose your hand (Rock/Paper/Scissors):");
-
         do{
             if (typeof userGameInput === "string"){
-                userGameInput = userGameInput.toLowerCase();
+                userGameInput = userGameInput.toLowerCase().trim();
                 if(userGameInput == "rock" || userGameInput == "paper" || userGameInput == "scissors"){
                     break;
                 }
                 else{
                     userGameInput = prompt("Please write either Rock/Paper/Scissors");
                 }
+            }
+            else if(typeof userGameInput ==="object"){
+                throw new Error("You're Ending the Game");
             }
             else{
                 userGameInput = prompt("Please write either Rock/Paper/Scissors");
@@ -78,18 +79,17 @@ function game(){
         while(true);
 
         const computerSelection = computerPlay();
-        const roundResult = playRound(userGameInput,computerSelection);
+        const roundResult = processingTheRoundWinner(userGameInput,computerSelection);
 
         alert(`You Choose ${userGameInput}\nThe Computer Choose ${computerSelection}\nThe Result:\n${roundResult}`);
     }
+}
 
-    alert(`Game Over!\nHere are your Results:\nWin: ${userWinCount}x\nLose: ${userLoseCount}x\nTie: ${userTieCount}x`);
-    
+function askUserForReplay(){
     let askingToPlayAgain = prompt("Do you want to Play Again? (Answer: yes/no)");
-    // Asking the User to Play Again or not
     do { 
         if(typeof askingToPlayAgain === "string"){
-            askingToPlayAgain = askingToPlayAgain.toLowerCase();
+            askingToPlayAgain = askingToPlayAgain.toLowerCase().trim();
             if(askingToPlayAgain == "yes"){
                 userLoseCount = 0;
                 userTieCount = 0;
@@ -103,10 +103,23 @@ function game(){
                 askingToPlayAgain = prompt("Please write either yes/no");
             }
         }
+        else if(typeof askingToPlayAgain ==="object"){
+            throw new Error("You're Ending the Game");
+        }
         else{
             askingToPlayAgain = prompt("Please write either yes/no");
         }
     } while (true);
+}
+
+function game(){    
+    alert("Time to Play Rock-Paper-Scissors!!\nThis is going to be a Five Round Match againts a Computer\nAre you Ready??");
+    
+    playFiveRound();
+
+    alert(`Game Over!\nHere are your Results:\nWin: ${userWinCount}x\nLose: ${userLoseCount}x\nTie: ${userTieCount}x`);
+    
+    askUserForReplay();
 }
 
 game();
